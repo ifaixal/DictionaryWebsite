8a6a678033aab5b27e739bc4fc9e2740223076bf
ifaixal <fai.iqbal124@gmail.com>
2025-06-19 19:28:51 +0500

    Styling along with getting the data

---
+++ Script.js
@@ -0,0 +1,27 @@
+// function searchWord()
+// {
+//     let userInput = document.getElementById("SearchWord").value;
+    
+//     fetchWord(userInput);
+    
+// }
+
+// function keydown(e)
+// {
+//     if(e.keyCode === 13) {
+//         searchWord();
+//       }
+// }
+
+// async function fetchWord(word)
+// {
+//     let url = "https://api.dictionaryapi.dev/api/v2/entries/en/";
+//     url += word;
+//     let response = await fetch(url);
+
+//     if (!response.ok){
+//         console.log("No Word Found");
+//     }
+        
+//     let data = await response.json();
+// }
